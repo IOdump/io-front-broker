442d2a75ece55afd85a801aad004430738366496
Mathieu Elie <mathieuelie@gmail.com>
2014-01-03 15:34:26 +0100

    using event emmiter for reusing amqp connections

--- app.js
+++ app.js
@@ -2,11 +2,45 @@ var app = require('http').createServer(handler)
   , io = require('socket.io').listen(app)
   , fs = require('fs')
   , amqp          = require('amqp');
+  , events = require('events');
 
 
 var connection = amqp.createConnection({ host: '127.0.0.1' }, { reconnect: true });
 
+// reduce io logging
+io.set('log level', 1);
 app.listen(3100);
+
+var eventEmitter = new events.EventEmitter();
+
+connection.on('ready', function () {
+
+  console.log('rabbitmq connected !');
+  var exchange = connection.exchange('realtime');
+  var queue = connection.queue('io-front-broker');
+
+  queue.bind(exchange, '#');
+
+
+    queue.subscribe(function (message) {
+      // console.log(message);
+      eventEmitter.emit('rtdata', message);
+
+    });
+
+
+});
+
+
+io.sockets.on('connection', function (socket) {
+  console.log('io socket connection');
+
+  eventEmitter.on('rtdata', function(message) {
+   socket.emit(message.topic, message);
+ });
+
+});
+
 
 function handler (req, res) {
   fs.readFile(__dirname + '/index.html',
@@ -20,21 +54,3 @@ function handler (req, res) {
     res.end(data);
   });
 }
-
-connection.on('ready', function () {
-
-  console.log('rabbitmq connected !');
-  var exchange = connection.exchange('realtime');
-  var queue = connection.queue('io-front-broker');
-
-  queue.bind(exchange, '#');
-
-  io.sockets.on('connection', function (socket) {
-    queue.subscribe(function (message) {
-      console.log(message);
-      socket.emit(message.topic, message);
-    });
-
-  });
-
-});
